53fa41f167d1e5d3b15c90f13229d2fec27cf8e3
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-10-13 12:18:17 +0200

    Adds basic module test

---
+++ test/modules-test.js
@@ -0,0 +1,10 @@
+const test = require('ava')
+
+test('requires options', t => {
+  const config = false
+  const error = t.throws(() => {
+    const svarut = require('../src/index')(config)
+    console.log(svarut)
+  }, Error)
+  t.is(error.message, 'Missing options')
+})
